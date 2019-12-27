5c179a14e8b313f46f2b9f6c85dfd4b154ce0c3f
Ricardo_Worf <steve4137@gmail.com>
2019-12-27 19:37:31 -0300

    criando a page

--- vendor/hcodebr/php-classes/src/DB/Sql.php
+++ vendor/hcodebr/php-classes/src/DB/Sql.php
@@ -7,7 +7,7 @@ class Sql {
 	const HOSTNAME = "localhost";
 	const USERNAME = "root";
 	const PASSWORD = "";
-	const DBNAME = "db_ecommerce";
+	const DBNAME   = "db_ecommerce";
 
 	private $conn;
 
---
+++ views-cache/header.003873c670aac278cd0a8e0d7f83d824.rtpl.php
@@ -0,0 +1,9 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+	<body>
+		
